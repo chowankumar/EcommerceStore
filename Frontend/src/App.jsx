968890c6d434f95599b5f9abada255715fc03679
chowankumar <chowanmalhani5@gmail.com>
2024-07-30 00:24:51 +0500

    ad functionality when we click on the button we will go to that route

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
         <Route path='/womens' element={<ShopCategory category = "women"/>} />
         <Route path='/kids' element={<ShopCategory category = "kid"/>} />
         <Route path='/product' element={<Product/>}>
-        <Route path='/:productId' element={<Product/>} />
+        <Route path=':productId' element={<Product/>} />
         </Route>
 
         <Route path='/cart' element={<Cart/>} />
