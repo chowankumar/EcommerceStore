646ca1a935bee9d2ca8886564e48f4145ba7d1e1
chowankumar <chowanmalhani5@gmail.com>
2024-08-21 20:17:02 +0500

    set up the context to the all over project

---
+++ frontend/src/components/LatestCollection.jsx
@@ -0,0 +1,14 @@
+import React, { useContext } from 'react'
+import {ShopConext} from '../context/ShopContext'
+
+const LatestCollection = () => {
+
+    const {products} = useContext(ShopConext)
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default LatestCollection
