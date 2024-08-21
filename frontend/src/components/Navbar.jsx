4702e9fd68e40e53ac138b499e0aec2e981a3dce
chowankumar <chowanmalhani5@gmail.com>
2024-08-21 16:04:13 +0500

    complete the navabr  component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets } from "./../assets/assets"
 import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+
+    const [visible,setVisible] = useState(true);
     return (
         <div className='flex items-center justify-between py-5 font-medium'>
             <img src={assets.logo} alt="" className='w-36' />
@@ -69,9 +71,31 @@ const Navbar = () => {
                 </Link>
                 <img 
                 src={assets.menu_icon}
-                className='w-5 cursor-pointer sm:hidden' />
+                className='w-5 cursor-pointer sm:hidden'
+                onClick={()=>setVisible(true)} />
 
 
+
+            </div>
+
+            {/* menu for the small screens */}
+
+            <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transiton-all ${visible ? 'w-full':'w-0'}`}>
+            <div className="flex flex-col text-gray-600"> 
+                <div onClick={()=> setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
+                    <img 
+                    src={assets.dropdown_icon}
+                    className='h-4 rotate-180'
+                  
+                    />
+                    <p>back</p>
+
+                </div>
+            <NavLink onClick={()=> setVisible(false)} className="py-2 pl-6 border" to='/'>HOME</NavLink>
+            <NavLink onClick={()=> setVisible(false)} className="py-2 pl-6 border" to='/collectio'>COLLECTION</NavLink>
+            <NavLink onClick={()=> setVisible(false)} className="py-2 pl-6 border" to='/about'>ABOUT</NavLink>
+            <NavLink onClick={()=> setVisible(false)} className="py-2 pl-6 border" to='/contact'>CONTACT</NavLink>
+            </div>
 
             </div>
 
