ca087ac948d65a3935aba641f442f38a424ff658
chowankumar <chowanmalhani5@gmail.com>
2024-08-22 20:45:53 +0500

    fetch 10 product from prduct and listed on the latest collection component

--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -6,7 +6,7 @@ const ProductItem = ({id,image,name,price}) => {
     
     const {currency} = useContext(ShopContext);
   return (
-    <link className='text-gray-700 cursor-pointer' to={`/product/${id}`}>
+    <Link className='text-gray-700 cursor-pointer' to={`/product/${id}`}>
      <div className='overflow-hidden'>
         <img 
         src={image[0]}
@@ -15,7 +15,7 @@ const ProductItem = ({id,image,name,price}) => {
      </div>
      <p className='pt-3 pb-1 text-sm'>{name}</p>
      <p className='text-sm font-medium'>{currency}{price}</p>
-    </link>
+    </Link>
    
   )
 }
