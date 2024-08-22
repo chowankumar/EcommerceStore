db573a6f84766d6e34754ee1ff482fde06314d86
chowankumar <chowanmalhani5@gmail.com>
2024-08-22 22:34:20 +0500

    create the ourPolicy Component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
+import OurPolicy from '../components/ourPolicy'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero/>
       <LatestCollection/>
       <BestSeller/>
+      <OurPolicy/>
     </div>
   )
 }
