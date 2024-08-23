c66ed30da3be2721a28da9bad88c885a7fc07099
chowankumar <chowanmalhani5@gmail.com>
2024-08-23 12:41:18 +0500

    set the category section for the mobile and small screens

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from 'react'
 import { ShopContext } from ".././context/ShopContext"
+import { assets } from '../assets/assets'
 
 const Collection = () => {
   const { products } = useContext(ShopContext)
@@ -8,7 +9,11 @@ const Collection = () => {
     <div className='flex flex-col sm:flex-row gap-1 s:gap-10 pt-10 border-t'>
 
       <div className="min-w-60">
-        <p className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS</p>
+        <p onClick={()=> setShowFilter(!showFilter)} className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS
+          <img 
+           src={assets.dropdown_icon}
+           className={`h-3 sm:hidden ${showFilter?'rotate-90':""}`} />
+        </p>
 
         <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? "" : "hidden"} sm:block`}>
           <p className='mb-3 text-sm font-medium'>CATERORIES</p>
