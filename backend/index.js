9c85ed673d0ac2338f2af23758292a1bccaae5bb
chowankumar <chowanmalhani5@gmail.com>
2024-08-30 02:03:25 +0500

    uplaod picture of the products

--- backend/index.js
+++ backend/index.js
@@ -8,22 +8,48 @@ const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
 
-
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb+srv://chowankumar:chowan54@cluster0.tlfso.mongodb.net/");
+mongoose.connect("mongodb+srv://chowankumar:chowan54@cluster0.tlfso.mongodb.net/", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((err) => {
+    console.error("MongoDB connection error: ", err);
+});
 
-app.get("/",(req,res)=>{
-    res.send("express app is running")
+app.get("/", (req, res) => {
+    res.send("express app is running");
+});
 
-})
+///////////////////////////////////////////////////////
 
-app.listen(port,(error)=>{
-    if(!error){
-        console.log(`server running on the port ${port} `)
-    }else{
-        console.log("error in database connection" + error)
+// Image storage function
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename: (req, file, cb) => {
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
     }
+});
 
-})
+const upload = multer({ storage: storage });
+app.use('/images', express.static('upload/images'));
+
+app.post("/upload", upload.single('product'), (req, res) => {
+    res.json({
+        success: 1,
+        image_url: `http://localhost:${port}/images/${req.file.filename}`
+    });
+});
+
+/////////////////////////////////////////////////////////////
+
+app.listen(port, (error) => {
+    if (!error) {
+        console.log(`Server running on port ${port}`);
+    } else {
+        console.log("Error in database connection: " + error);
+    }
+});
