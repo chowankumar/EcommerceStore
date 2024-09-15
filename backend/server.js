8ff53f43185fb59f3221e1df4fe9956f2ab9df6a
chowan kumar <chowanmalhani5@gmail.com>
2024-09-15 23:44:42 +0500

    create the connection of the cloudinary

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb'
+import connectCloudinary from './config/cloudinary'
+
+
+//app config
+const app = express()
+const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
+
+
+//middlewares
+app.use(express.json())
+app.use(cors())
+
+//api endpoints
+
+app.get('/',(req,res)=>{
+    res.send('API WORKING')
+
+})
+app.listen(port,()=> console.log('server started on port :' + port))
