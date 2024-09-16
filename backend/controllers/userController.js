2acde96b63dfa0ee03192d5b0346894360e910d6
chowan kumar <chowanmalhani5@gmail.com>
2024-09-16 19:30:36 +0500

    create the useRoutes

---
+++ backend/controllers/userController.js
@@ -0,0 +1,15 @@
+
+
+const loginUser = async(req,res)=>{
+
+}
+
+const registerUser= async(req,res)=>{
+
+}
+
+const adminLogin = async(req,res)=>{
+
+}
+
+export {loginUser,registerUser,adminLogin}
