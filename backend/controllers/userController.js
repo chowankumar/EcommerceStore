5bb061feab99777b866616662e3fd1484f2b6726
chowan kumar <chowanmalhani5@gmail.com>
2024-09-20 00:36:14 +0500

    create admin controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -73,7 +73,24 @@ const registerUser = async (req, res) => {
 
 }
 
-const adminLogin = async (req, res) => {
+const adminLogin = async (req,res) => {
+    try {
+        const {email,password} = req.body;
+
+        if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
+            const token = jwt.sign(email+password,process.env.JWT_SECRET)
+
+            res.json({succes:true,token})
+        }else{
+            res.json({success:false,message:"invalid credentials"})
+
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message})
+
+        
+    }
     
 
 }
