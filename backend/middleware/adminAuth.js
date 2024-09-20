699c46428b5a0447378e6bc7b055efffd55c923d
chowan kumar <chowanmalhani5@gmail.com>
2024-09-20 11:46:15 +0500

    complete the adminAuth middleware

---
+++ backend/middleware/adminAuth.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken'
+
+const adminAuth = async (req,res,next)=>{
+    try {
+        const token = req.headers
+        if(!token){
+            return res.json({success:false,message:"Not Authorized Login Again"})
+        }
+        const decode_token = jwt.verify(token,process.env.JWT_SECRET)
+
+        if(decode_token !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWOD){
+            return res.json({success:false,message:"Not Authorized Login Again"})
+        }
+        next();
+     } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message})
+        
+    }
+}
