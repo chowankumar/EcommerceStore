8a0d36ae3af852db77c213d71aa00c52037eb3b9
chowan kumar <chowanmalhani5@gmail.com>
2024-09-23 02:48:20 -0700

    create the conroller for status update

--- admin/src/pages/Order.jsx
+++ admin/src/pages/Order.jsx
@@ -30,6 +30,20 @@ const Order = ({ token }) => {
 
   }
 
+  const statusHandler = async(event,orderId)=>{
+    try {
+      const response = await axios.post(backendUrl + '/api/order/status',{orderId,status:event.target.value},{headers:{token}})
+      if(response.data.success){
+        await fetchAllOrders();
+      }
+       
+    } catch (error) {
+      console.log(error);
+      toast.error(response.data.message)
+      
+    }
+  }
+
 
   useEffect(() => {
     if (token) {
@@ -74,7 +88,7 @@ const Order = ({ token }) => {
                  
               </div>
               <p className='text-sm sm:text-[15px]'>{currency}{order.amount}</p>
-              <select value={order.status} className='p-2 font-semibold' name="" id="">
+              <select onChange={(event)=> statusHandler(event,order._id)} value={order.status} className='p-2 font-semibold' name="" id="">
                 <option value="Order Placed">Order Placed</option>
                 <option value="Packing">Packing</option>
                 <option value="Shipped">Shipped</option>
