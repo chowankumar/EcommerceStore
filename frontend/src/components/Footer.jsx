ee932e5e3ab58fee157a8f63f4fb83a855376cc1
chowan kumar <chowanmalhani5@gmail.com>
2024-09-24 05:38:45 -0700

    change the content of the footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,12 +3,12 @@ import {assets} from "../assets/assets"
 const Footer = () => {
   return (
     <div>
-        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm '>
             
          <div>
-         <img src={assets.logo} alt="" className='mb-5 w-32' />
+         <img src={assets.logo} alt="" className='w-[200px]' />
             <p className='w-full md:w-2/3 text-gray-600'>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, blanditiis?</p>
+            Your one-stop shop for everything you love. Discover, shop, and enjoy—because you deserve the best!</p>
          </div>
 
          <div>
