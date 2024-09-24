107ab2c3ee75ecdb510ecad31bf742bc43b046c7
chowan kumar <chowanmalhani5@gmail.com>
2024-09-24 02:07:30 -0700

    create my ow logo and add it to the navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
     }
     return (
         <div className='flex items-center justify-between py-5 font-medium'>
-            <Link to='/'><img src={assets.logo} alt="" className='w-36' /></Link>
+            <Link to='/'><img src={assets.logo} alt="" className='w-[270px]' /></Link>
             
             <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
 
