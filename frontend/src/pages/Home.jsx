ac4004753f14e3c24f661d070f1fbd35c43b7402
chowan kumar <chowanmalhani5@gmail.com>
2024-09-27 00:58:30 -0700

    add our policy

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import OurPolicy from "./../components/OurPolicy"
 import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
 import NewsLetterBox from '../components/NewsLetterBox'
