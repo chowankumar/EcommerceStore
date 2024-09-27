967a25b61c0ee3f9712afca5ebc1bce4678f4287
chowan kumar <chowanmalhani5@gmail.com>
2024-09-27 00:46:49 -0700

    done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
-import OurPolicy from '../components/ourPolicy'
 import NewsLetterBox from '../components/NewsLetterBox'
 import Slider from '../components/Slider'
 
